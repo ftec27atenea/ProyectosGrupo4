ba2d5648539ceda54329c4aba471177f9bc14901
mdazag <41237965+mdazag@users.noreply.github.com>
2023-07-31 10:31:24 -0500

    Subir ejercicios

--- app/src/main/java/com/example/proyectosgrupo4/MainActivity.java
+++ app/src/main/java/com/example/proyectosgrupo4/MainActivity.java
@@ -13,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sesion_32);
+
         TextView textView=findViewById(R.id.TextoEnPantalla);
+        //Animation animation= AnimationUtils;
+
     }
 }
