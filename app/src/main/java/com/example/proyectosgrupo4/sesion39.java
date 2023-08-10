f405b43af4be8f8efed4cf8e4c81e5768db559ef
martin <mdazag@gmail.com>
2023-08-10 14:16:56 -0500

    Ejercicio sesión 39

--- app/src/main/java/com/example/proyectosgrupo4/sesion39.java
+++ app/src/main/java/com/example/proyectosgrupo4/sesion39.java
@@ -3,16 +3,12 @@ package com.example.proyectosgrupo4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
-
-public class sesion_37 extends AppCompatActivity {
+public class sesion39 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sesion_37);
-        ImageView ivGif=findViewById(R.id.Biblia);
+        setContentView(R.layout.sesion_39);
     }
 }
